b9f76e2e7dd010a52391c0dba9cf1719b3ee9576
mizdra <pp.mizdra@gmail.com>
2020-12-19 22:36:01 +0900

    chore: try to use eslint API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('hello world');
+import { ESLint } from 'eslint';
+
+(async function main() {
+  // 1. Create an instance.
+  const eslint = new ESLint({});
+
+  // 2. Lint files.
+  const results = await eslint.lintFiles(['src']);
+
+  // 3. Format the results.
+  const formatter = await eslint.loadFormatter('stylish');
+  const resultText = formatter.format(results);
+
+  // 4. Output it.
+  console.log(resultText);
+})().catch((error) => {
+  process.exitCode = 1;
+  console.error(error);
+});
