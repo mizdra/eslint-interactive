0cdeeb460c49f873d3a7f65ecdb5ed3eabdf6af0
mizdra <pp.mizdra@gmail.com>
2020-12-19 19:32:53 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+// @ts-check
+
+/** @type import('eslint').Linter.BaseConfig */
+module.exports = {
+  root: true,
+  extends: [
+    // basic
+    '@mizdra/mizdra',
+    '@mizdra/mizdra/+typescript',
+    '@mizdra/mizdra/+prettier',
+  ],
+  env: {
+    node: true,
+  },
+};
