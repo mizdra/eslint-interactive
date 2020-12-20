aad359b2bade7c8609dbcfc19e6ff7c4149356e3
mizdra <pp.mizdra@gmail.com>
2020-12-20 18:06:30 +0900

    feat: supports repeating actions

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
 
-import { ESLint, Linter } from 'eslint';
 import yargs from 'yargs/yargs';
 import { fix, lint, showMessages } from './eslint';
-import { calcFormattedChoices } from './eslint-formatter/stats';
 import { format as formatBySummary } from './eslint-formatter/summary';
 import { prompt } from './prompt';
-import { calcRuleResults } from './stat';
 
 const argv = yargs(process.argv.slice(2)).argv;
 // NOTE: convert `string` type because yargs convert `'10'` (`string` type) into `10` (`number` type)
@@ -14,23 +11,26 @@ const argv = yargs(process.argv.slice(2)).argv;
 const patterns = argv._.map((pattern) => pattern.toString());
 
 (async function main() {
-  const { eslint, results, ruleIdChoices } = await lint(patterns);
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    let { eslint, results, ruleIdChoices } = await lint(patterns);
+    console.log(formatBySummary(results));
 
-  console.log(formatBySummary(results));
+    const answers = await prompt(ruleIdChoices);
 
-  const answers = await prompt(ruleIdChoices);
-
-  if (answers.action === 'showMessages') {
-    await showMessages(eslint, results, answers);
-  } else if (answers.action === 'fix') {
-    const {
-      eslint: newESLint,
-      results: newResults,
-      ruleIdChoices: newRuleIdChoices,
-    } = await fix(patterns, answers);
+    if (answers.action === 'showMessages') {
+      await showMessages(eslint, results, answers);
+    } else if (answers.action === 'fix') {
+      const {
+        eslint: newESLint,
+        results: newResults,
+        ruleIdChoices: newRuleIdChoices,
+      } = await fix(patterns, answers);
+      eslint = newESLint;
+      results = newResults;
+      ruleIdChoices = newRuleIdChoices;
+    }
   }
-
-  console.info(answers);
 })().catch((error) => {
   process.exitCode = 1;
   console.error(error);
