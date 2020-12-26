4d10e2fc76fc39373ad97e0b4537b4ea6f329be8
mizdra <pp.mizdra@gmail.com>
2020-12-26 17:29:15 +0900

    chore: use ink

---
+++ src/components/App.tsx
@@ -0,0 +1,18 @@
+import { Text } from 'ink';
+import React, { useState, useEffect } from 'react';
+
+export function App() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCounter((previousCounter) => previousCounter + 1);
+    }, 100);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return <Text color="green">{counter} tests passed</Text>;
+}
