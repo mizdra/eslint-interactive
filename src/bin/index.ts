9e9c99a1044a6d973f386db06eee86fc788e32c6
mizdra <pp.mizdra@gmail.com>
2020-12-30 20:19:43 +0900

    chore: improve something

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -2,6 +2,7 @@
 
 import chalk from 'chalk';
 import ora from 'ora';
+import sourceMapSupport from 'source-map-support';
 import yargs from 'yargs/yargs';
 import { CachedESLint } from './cached-eslint';
 import {
@@ -9,6 +10,8 @@ import {
   promptToInputContinue,
   promptToInputRuleIds,
 } from './prompt';
+
+sourceMapSupport.install();
 
 const argv = yargs(process.argv.slice(2)).argv;
 // NOTE: convert `string` type because yargs convert `'10'` (`string` type) into `10` (`number` type)
