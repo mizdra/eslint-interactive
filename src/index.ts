2ec35e011d8f74241a2853cebdec4c29823d12b7
mizdra <pp.mizdra@gmail.com>
2021-11-24 02:22:56 +0900

    refactor: create parse-argv module

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import yargs from 'yargs/yargs';
+import { parseArgv } from './cli/parse-argv';
 import { CachedESLint } from './eslint';
 import { selectAction } from './scenes/select-action';
 import { selectRuleIds } from './scenes/select-rule-ids';
@@ -11,33 +12,7 @@ export type Options = {
 };
 
 export async function run(options: Options) {
-  const argv = yargs(options.argv.slice(2))
-    .usage('$0 [file.js] [dir]')
-    .option('rulesdir', {
-      type: 'array',
-      describe: 'Use additional rules from this directory',
-    })
-    .nargs('rulesdir', 1)
-    .option('ext', {
-      type: 'array',
-      describe: 'Specify JavaScript file extensions',
-    })
-    .nargs('ext', 1)
-    .option('format', {
-      type: 'string',
-      describe: 'Specify the format to be used for the `Display problem messages` action',
-      default: 'codeframe',
-    }).argv;
-  // NOTE: convert `string` type because yargs convert `'10'` (`string` type) into `10` (`number` type)
-  // and `lintFiles` only accepts `string[]`.
-  const patterns = argv._.map((pattern) => pattern.toString());
-  const rulePaths = argv.rulesdir?.map((rulePath) => rulePath.toString());
-  const extensions = argv.ext
-    ?.map((extension) => extension.toString())
-    // map '.js,.ts' into ['.js', '.ts']
-    .flatMap((extension) => extension.split(','));
-  const formatterName = argv.format;
-
+  const { patterns, rulePaths, extensions, formatterName } = parseArgv(options.argv);
   const eslint = new CachedESLint(patterns, { rulePaths, extensions });
 
   let nextScene: NextScene = { name: 'showLintResults' };
