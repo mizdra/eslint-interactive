2f40373b510801d124746a1cdca893869d5b5a65
mizdra <pp.mizdra@gmail.com>
2021-12-14 00:00:00 +0900

    fix: support ESM for jest and ts-jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,17 +4,20 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   displayName: 'test',
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testMatch: ['<rootDir>/test/**/*.test.ts?(x)'],
   // ESLint v8 では `pkg.exports` を利用したモジュールに依存しているが、jest は `pkg.exports` を解釈できないため、
   // そのままだと jest がコケてしまう。そこで `moduleNameMapper` を使って無理やりモジュール名を解決している。
   moduleNameMapper: {
     '@eslint/eslintrc/universal': '@eslint/eslintrc/dist/eslintrc-universal.cjs',
+    // Map `./**/xxx.js` to `./**/xxx` (for ESM)
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json',
+      useESM: true,
     },
   },
 };
