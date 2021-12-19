c6a2c790494fb6b5ce2a22215da3a88fb784743c
mizdra <pp.mizdra@gmail.com>
2021-12-19 23:31:38 +0900

    fix: make jest ESM-ready

--- jest.config.mjs
+++ jest.config.mjs
@@ -5,17 +5,23 @@
 // eslint-disable-next-line import/no-default-export
 export default {
   displayName: 'test',
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testMatch: ['<rootDir>/test/**/*.test.ts?(x)'],
   // ESLint v8 では `pkg.exports` を利用したモジュールに依存しているが、jest は `pkg.exports` を解釈できないため、
   // そのままだと jest がコケてしまう。そこで `moduleNameMapper` を使って無理やりモジュール名を解決している。
   moduleNameMapper: {
-    '@eslint/eslintrc/universal': '@eslint/eslintrc/dist/eslintrc-universal.cjs',
+    // Map `./**/xxx.js` to `./**/xxx` (for ESM)
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
+  // for ESM
+  resolver: '<rootDir>/test/test-util/jest/resolver.cjs',
+  // do not transform `import` statements (for ESM)
+  transform: {},
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json',
+      useESM: true,
     },
   },
 };
