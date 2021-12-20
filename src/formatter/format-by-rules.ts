f6f2135e6780b6437afc5c6fb0fda5fc6f655134
mizdra <pp.mizdra@gmail.com>
2021-12-20 23:56:15 +0900

    feat(BREAKING CHANGES): disable documentation links temporarily

--- src/formatter/format-by-rules.ts
+++ src/formatter/format-by-rules.ts
@@ -1,11 +1,11 @@
 import chalk from 'chalk';
 import Table from 'cli-table';
 import { ESLint } from 'eslint';
-import terminalLink from 'terminal-link';
+// import terminalLink from 'terminal-link';
 import { ERROR_COLOR, WARNING_COLOR } from './colors';
 import { takeRuleStatistics } from './take-rule-statistics';
 
-export const formatByRules: ESLint.Formatter['format'] = (results, data) => {
+export const formatByRules: ESLint.Formatter['format'] = (results, _data) => {
   const ruleStatistics = takeRuleStatistics(results);
   const table = new Table({
     head: ['Rule', 'Error (fixable/suggest-applicable)', 'Warning (fixable/suggest-applicable)'],
@@ -21,9 +21,12 @@ export const formatByRules: ESLint.Formatter['format'] = (results, data) => {
       suggestApplicableErrorCount,
       suggestApplicableWarningCount,
     } = ruleStatistic;
-    const ruleMetaData = data?.rulesMeta[ruleId];
 
-    const ruleCell = ruleMetaData?.docs?.url ? terminalLink(ruleId, ruleMetaData?.docs.url) : ruleId;
+    // NOTE: Disable documentation links temporarily due to problems with cli-table.
+    // ref: https://github.com/mizdra/eslint-interactive/issues/81
+    // const ruleMetaData = data?.rulesMeta[ruleId];
+    // const ruleCell = ruleMetaData?.docs?.url ? terminalLink(ruleId, ruleMetaData?.docs.url) : ruleId;
+    const ruleCell = ruleId;
     let errorCell = `${errorCount} (${fixableErrorCount}/${suggestApplicableErrorCount})`;
     if (errorCount > 0) errorCell = chalk[ERROR_COLOR].bold(errorCell);
     let warningCell = `${warningCount} (${fixableWarningCount}/${suggestApplicableWarningCount})`;
