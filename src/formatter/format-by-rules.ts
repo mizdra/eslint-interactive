ddb69a442b60cf73bcfe1d6cf9c3728a67f25183
mizdra <pp.mizdra@gmail.com>
2021-12-26 02:43:37 +0900

    feat: improve statistic table

--- src/formatter/format-by-rules.ts
+++ src/formatter/format-by-rules.ts
@@ -2,36 +2,34 @@ import chalk from 'chalk';
 import Table from 'cli-table';
 import { ESLint } from 'eslint';
 // import terminalLink from 'terminal-link';
-import { ERROR_COLOR, WARNING_COLOR } from './colors';
+import { ERROR_COLOR } from './colors';
 import { takeRuleStatistics } from './take-rule-statistics';
+
+function numCell(num: number): string {
+  return num > 0 ? chalk[ERROR_COLOR].bold(num) : num.toString();
+}
 
 export const formatByRules: ESLint.Formatter['format'] = (results, _data) => {
   const ruleStatistics = takeRuleStatistics(results);
   const table = new Table({
-    head: ['Rule', 'Error (fixable/suggest-applicable)', 'Warning (fixable/suggest-applicable)'],
+    head: ['Rule', 'Error', 'Warning', 'is fixable', 'has suggestions'],
   });
 
   ruleStatistics.forEach((ruleStatistic) => {
-    const {
-      ruleId,
-      errorCount,
-      warningCount,
-      fixableErrorCount,
-      fixableWarningCount,
-      suggestApplicableErrorCount,
-      suggestApplicableWarningCount,
-    } = ruleStatistic;
+    const { ruleId, errorCount, warningCount, fixableCount, suggestApplicableCount } = ruleStatistic;
 
     // NOTE: Disable documentation links temporarily due to problems with cli-table.
     // ref: https://github.com/mizdra/eslint-interactive/issues/81
     // const ruleMetaData = data?.rulesMeta[ruleId];
     // const ruleCell = ruleMetaData?.docs?.url ? terminalLink(ruleId, ruleMetaData?.docs.url) : ruleId;
     const ruleCell = ruleId;
-    let errorCell = `${errorCount} (${fixableErrorCount}/${suggestApplicableErrorCount})`;
-    if (errorCount > 0) errorCell = chalk[ERROR_COLOR].bold(errorCell);
-    let warningCell = `${warningCount} (${fixableWarningCount}/${suggestApplicableWarningCount})`;
-    if (warningCount > 0) warningCell = chalk[WARNING_COLOR].bold(warningCell);
-    table.push([ruleCell, errorCell, warningCell]);
+    table.push([
+      ruleCell,
+      numCell(errorCount),
+      numCell(warningCount),
+      numCell(fixableCount),
+      numCell(suggestApplicableCount),
+    ]);
   });
 
   return table.toString();
