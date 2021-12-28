36e4c547b8bc4093375bd19f01f94609d7f7a3df
mizdra <pp.mizdra@gmail.com>
2021-12-28 23:35:57 +0900

    build: include `.cjs`, `.mjs`, `.cts` and `.mts` as lint targets

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,7 +21,7 @@ module.exports = {
   overrides: [
     // for typescript
     {
-      files: ['*.ts', '*.tsx'],
+      files: ['*.ts', '*.tsx', '*.cts', '*.mts'],
       parserOptions: {
         project: ['./tsconfig.src.json', './tsconfig.test.json'],
       },
@@ -39,7 +39,7 @@ module.exports = {
     },
     // for test
     {
-      files: ['test/**/*.{ts,tsx}'],
+      files: ['test/**/*.{ts,tsx,cts,mts}'],
       env: {
         jest: true,
       },
