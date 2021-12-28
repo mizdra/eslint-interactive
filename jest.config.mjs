c550aec44fa4e5242fcd7ea027bf2072b67fe964
mizdra <pp.mizdra@gmail.com>
2021-12-29 00:14:18 +0900

    fix: make jest ESM-ready

--- jest.config.mjs
+++ jest.config.mjs
@@ -5,19 +5,23 @@
 // eslint-disable-next-line import/no-default-export
 export default {
   displayName: 'test',
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testMatch: ['<rootDir>/test/**/*.test.ts?(x)'],
   // Since fixtures-tmp are rewritten during testing, if they are in the watch target, the test will be retrying infinitely.
   watchPathIgnorePatterns: ['<rootDir>/fixtures-tmp/'],
   // ESLint v8 では `pkg.exports` を利用したモジュールに依存しているが、jest は `pkg.exports` を解釈できないため、
   // そのままだと jest がコケてしまう。そこで `moduleNameMapper` を使って無理やりモジュール名を解決している。
   moduleNameMapper: {
-    '@eslint/eslintrc/universal': '@eslint/eslintrc/dist/eslintrc-universal.cjs',
+    // Map `./**/xxx.js` to `./**/xxx` (for ESM)
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
+  // do not transform `import` statements (for ESM)
+  transform: {},
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json',
+      useESM: true,
     },
   },
 };
