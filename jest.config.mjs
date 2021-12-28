2b4bd5ef7e71a277eb3c8bb8a6555d274f202c31
mizdra <pp.mizdra@gmail.com>
2021-12-29 00:26:55 +0900

    fix: include `.cjs`, `.mjs`, `.cts` and `.mts` as coverage targets

--- jest.config.mjs
+++ jest.config.mjs
@@ -19,7 +19,7 @@ export default {
   resolver: '<rootDir>/test/test-util/jest/resolver.cjs',
   // do not transform `import` statements (for ESM)
   transform: {},
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx,cjs,mjs,ts,tsx,cts,mts}', '!**/*.d.ts'],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json',
