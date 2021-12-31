7470c41495718bd9b1f7ecdaa274ab344583669e
mizdra <pp.mizdra@gmail.com>
2022-01-01 02:50:50 +0900

    feat: export Programmable API

--- src/formatter/index.ts
+++ src/formatter/index.ts
@@ -2,6 +2,8 @@ import { ESLint } from 'eslint';
 import { formatByFiles } from './format-by-files.js';
 import { formatByRules } from './format-by-rules.js';
 
+export { takeRuleStatistics } from './take-rule-statistics.js';
+
 export const format: ESLint.Formatter['format'] = (results, data) => {
   return formatByFiles(results) + '\n' + formatByRules(results, data);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 export { run, type Options } from './cli/run.js';
+export { Core } from './core.js';
+export { type Config } from './types.js';
+export { type FixableMaker, type SuggestionFilter } from './plugin/index.js';
+export { takeRuleStatistics } from './formatter/index.js';
