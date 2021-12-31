a84d0e6e99cb61c3dba6088698814a9288a55cb8
mizdra <pp.mizdra@gmail.com>
2022-01-01 01:28:13 +0900

    refactor: import from index.js

---
+++ src/scenes/index.ts
@@ -0,0 +1,4 @@
+export { lint } from './lint.js';
+export { selectAction, type SelectActionArgs } from './select-action.js';
+export { selectRuleIds, type SelectRuleIdsArgs } from './select-rule-ids.js';
+export { selectToContinue } from './select-to-continue.js';
