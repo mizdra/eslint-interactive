144673abd084a08654505ebc11d289d99e12e8d6
mizdra <pp.mizdra@gmail.com>
2022-02-04 00:17:12 +0900

    fix: fix e2e test runner

--- run-e2e-test.sh
+++ run-e2e-test.sh
@@ -2,6 +2,8 @@
 set -eu
 cd `dirname $0`
 
+ROOT_DIR=`pwd`
+
 test_cases=(`ls e2e-test/*/test.sh`)
 
 for test_case in "${test_cases[@]}"; do
@@ -9,4 +11,5 @@ for test_case in "${test_cases[@]}"; do
   cd $test_case_dir
   echo "+ Running test case: $test_case_dir"
   ./test.sh
+  cd $ROOT_DIR
 done
