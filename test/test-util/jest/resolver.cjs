3b079ca18380778badafe9e3c49666abea3b80c5
mizdra <pp.mizdra@gmail.com>
2022-02-04 01:16:09 +0900

    Revert "chore: respect `NODE_PATH`"
    This reverts commit ce0e3f008497a47f7db0fb0b70c0125d83b7a652.

--- test/test-util/jest/resolver.cjs
+++ test/test-util/jest/resolver.cjs
@@ -1,18 +1,9 @@
-const { delimiter } = require('path');
-
-const modules =
-  process.env.NODE_PATH && process.env.NODE_PATH.length > 0
-    ? [...process.env.NODE_PATH.split(delimiter), 'node_modules']
-    : ['node_modules'];
-
 // ref: https://github.com/facebook/jest/issues/9771#issuecomment-974750103
 const importResolver = require('enhanced-resolve').create.sync({
-  modules,
   conditionNames: ['import', 'node', 'default'],
   extensions: ['.js', '.json', '.node', '.ts'],
 });
 const requireResolver = require('enhanced-resolve').create.sync({
-  modules,
   conditionNames: ['require', 'node', 'default'],
   extensions: ['.js', '.json', '.node', '.ts'],
 });
