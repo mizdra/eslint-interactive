8c31c722922ac8273844a3a5917badd12a25448e
mizdra <pp.mizdra@gmail.com>
2022-02-25 01:59:35 +0900

    feat: enable rule documentation link

--- src/formatter/format-by-rules.ts
+++ src/formatter/format-by-rules.ts
@@ -2,7 +2,7 @@ import chalk from 'chalk';
 import { ESLint } from 'eslint';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 import table = require('table');
-// import terminalLink from 'terminal-link';
+import terminalLink from 'terminal-link';
 import { ERROR_COLOR } from './colors.js';
 import { takeRuleStatistics } from './take-rule-statistics.js';
 
@@ -18,7 +18,7 @@ function numCell(num: number): string {
   return num > 0 ? chalk[ERROR_COLOR].bold(num) : num.toString();
 }
 
-export function formatByRules(results: ESLint.LintResult[], _data?: ESLint.LintResultData): string {
+export function formatByRules(results: ESLint.LintResult[], data?: ESLint.LintResultData): string {
   const ruleStatistics = takeRuleStatistics(results);
 
   const rows: Row[] = [];
@@ -31,9 +31,8 @@ export function formatByRules(results: ESLint.LintResult[], _data?: ESLint.LintR
 
     // NOTE: Disable documentation links temporarily due to problems with cli-table.
     // ref: https://github.com/mizdra/eslint-interactive/issues/81
-    // const ruleMetaData = data?.rulesMeta[ruleId];
-    // const ruleCell = ruleMetaData?.docs?.url ? terminalLink(ruleId, ruleMetaData?.docs.url) : ruleId;
-    const ruleCell = ruleId;
+    const ruleMetaData = data?.rulesMeta[ruleId];
+    const ruleCell = ruleMetaData?.docs?.url ? terminalLink.stderr(ruleId, ruleMetaData?.docs.url) : ruleId;
     rows.push([
       ruleCell,
       numCell(errorCount),
