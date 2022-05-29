43150675c2b4c4f55adeef572fe88ebafd60a307
mizdra <pp.mizdra@gmail.com>
2022-05-29 15:49:43 +0900

    test: fix failed test

--- src/util/cache.ts
+++ src/util/cache.ts
@@ -1,6 +1,5 @@
 import { join } from 'path';
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-import cachedir = require('cachedir');
+import cachedir from 'cachedir';
 import { VERSION } from '../cli/package.js';
 
 /**
