8311f03bdfcda7633de5e3e138bbdbaaaaab8d4f
mizdra <pp.mizdra@gmail.com>
2022-06-04 12:17:48 +0900

    test: add workaround

--- e2e-test/import-as-esm-from-esm/jest.config.mjs
+++ e2e-test/import-as-esm-from-esm/jest.config.mjs
@@ -2,12 +2,11 @@
 
 import { baseConfig } from '../../jest.config.base.mjs';
 
-/** @typedef {import('ts-jest/dist/types')} */
-/** @type {import('@jest/types').Config.InitialOptions} */
-const config = {
-  ...baseConfig,
+/** @type {() => Promise<import('@jest/types').Config.InitialOptions>} */
+const config = async () => ({
+  ...(await baseConfig()),
   testMatch: ['<rootDir>/*.test.ts?(x)'],
-};
+});
 
 // eslint-disable-next-line import/no-default-export
 export default config;
--- jest.config.base.mjs
+++ jest.config.base.mjs
@@ -4,16 +4,18 @@ import { fileURLToPath } from 'url';
 
 const dir = join(dirname(fileURLToPath(import.meta.url)));
 
-/** @type import('@jest/types').Config.InitialOptions */
-export const baseConfig = {
+/** @type {() => Promise<import('@jest/types').Config.InitialOptions>} */
+export const baseConfig = async () => ({
   // Since fixtures-tmp and benchmark/fixtures are rewritten during testing, if they are in the watch target, the test will be retrying infinitely.
   watchPathIgnorePatterns: [join(dir, 'fixtures-tmp'), join(dir, 'benchmark/fixtures')],
   // for ESM
   extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
+    // workaround for https://github.com/facebook/jest/issues/12270
+    'chalk': fileURLToPath(await import.meta.resolve('chalk')),
+    '#ansi-styles': fileURLToPath(await import.meta.resolve('#ansi-styles', await import.meta.resolve('chalk'))),
+    '#supports-color': fileURLToPath(await import.meta.resolve('#supports-color', await import.meta.resolve('chalk'))),
     // Map `./**/xxx.js` to `./**/xxx` (for ESM)
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
-  // for ESM
-  resolver: join(dir, 'src/test-util/jest/resolver.cjs'),
-};
+});
