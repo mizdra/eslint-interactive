3882901afcc9888464fa8251b20a2ffce5a308f7
mizdra <pp.mizdra@gmail.com>
2023-02-04 17:45:37 +0900

    feat: create cache directory under cwd

--- src/util/cache.ts
+++ src/util/cache.ts
@@ -1,10 +1,15 @@
 import { join } from 'path';
 import cachedir from 'cachedir';
+import findCacheDirectory from 'find-cache-dir';
 import { VERSION } from '../cli/package.js';
 
 /**
  * Get the path of cache directory for eslint-interactive.
  */
 export function getCacheDir(): string {
-  return join(cachedir('eslint-interactive'), VERSION);
+  // If package.json exists in the parent directory of cwd,then node_modules/.cache/eslint-interactive
+  // under that directory is set as the cache directory.
+  // If it does not exist, the OS's cache directory is used.
+  const packageCacheDir = findCacheDirectory({ name: 'eslint-interactive' }) ?? cachedir('eslint-interactive');
+  return join(packageCacheDir, VERSION);
 }
