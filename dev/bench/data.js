c352d571f63afe06e29d51ad027d817883d60673
github-action-benchmark <github@users.noreply.github.com>
2023-02-11 13:00:25 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 28b87a00207f20aadd7de29673e239d52a16de78

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1676111801270,
+  "lastUpdate": 1676120425779,
   "repoUrl": "https://github.com/mizdra/eslint-interactive",
   "entries": {
     "Benchmark": [
@@ -9005,6 +9005,33 @@ window.BENCHMARK_DATA = {
             "name": "[OBSOLETE BENCHMARK] fix-overlapped-problems (undo, loop: 1)",
             "value": 0.4572080000070855,
             "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "mizdra",
+            "username": "mizdra",
+            "email": "pp.mizdra@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "28b87a00207f20aadd7de29673e239d52a16de78",
+          "message": "Merge pull request #264 from mizdra/stabilize-benchmark\n\nReliable benchmarking in noisy environments",
+          "timestamp": "2023-02-11T10:33:05Z",
+          "url": "https://github.com/mizdra/eslint-interactive/commit/28b87a00207f20aadd7de29673e239d52a16de78"
+        },
+        "date": 1676120424916,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "cachegrind-benchmarking",
+            "value": 20876072901,
+            "unit": "instructions"
           }
         ]
       }
