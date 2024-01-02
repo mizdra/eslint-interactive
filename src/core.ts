f22f537448e0a950e896d246d3e8fef555975d45
mizdra <pp.mizdra@gmail.com>
2024-01-03 01:35:00 +0900

    refactoring core

--- src/core.ts
+++ src/core.ts
@@ -11,13 +11,9 @@ import { createFixToConvertErrorToWarningPerFile } from './plugin/fix/convert-er
 import { createFixToDisablePerFile } from './plugin/fix/disable-per-file.js';
 import { createFixToDisablePerLine } from './plugin/fix/disable-per-line.js';
 import { createFixToMakeFixableAndFix } from './plugin/fix/make-fixable-and-fix.js';
-import { FixResult } from './plugin/fixer.js';
 import { FixableMaker, SuggestionFilter, FixContext } from './plugin/index.js';
-import { ruleFixer } from './plugin/rule-fixer.js';
-import { SourceCodeFixer } from './plugin/source-code-fixer.js';
+import { verifyAndFix } from './plugin/linter.js';
 import { filterResultsByRuleId } from './util/eslint.js';
-
-const MAX_AUTOFIX_PASSES = 10;
 
 /**
  * Generate results to undo.
@@ -180,54 +176,17 @@ export class Core {
     const linter = new Linter();
 
     // eslint-disable-next-line prefer-const
-    for (let { filePath, messages, source } of filteredResultsOfLint) {
+    for (let { filePath, source } of filteredResultsOfLint) {
       if (!source) throw new Error('Source code is required to apply fixes.');
       // eslint-disable-next-line no-await-in-loop
       const config: Linter.Config = await this.eslint.calculateConfigForFile(filePath);
 
-      // Get `SourceCode` instance
-      linter.verify(source, config, filePath);
-      let sourceCode = linter.getSourceCode();
-
-      let fixResult: FixResult | undefined;
-      for (let i = 0; i < MAX_AUTOFIX_PASSES; i++) {
-        // Create `Rule.Fix[]`
-        const fixContext: FixContext = {
-          filename: filePath,
-          sourceCode,
-          messages: messages.filter((message) => message.ruleId && ruleIds.includes(message.ruleId)),
-          ruleIds,
-          fixer: ruleFixer,
-        };
-        const fixes = fixCreator(fixContext);
-
-        // Apply `Rule.Fix[]`
-        fixResult = SourceCodeFixer.applyFixes(
-          sourceCode.text,
-          fixes.map((fix) => {
-            return {
-              ruleId: 'eslint-interactive/fix',
-              severity: 2,
-              message: 'fix',
-              line: 0,
-              column: 0,
-              fix,
-            };
-          }),
-          true,
-        );
-
-        // Lint `fixResult.output` again and check if there are any problems.
-        // If there are problems, retry to fix. If not, exit the loop.
-        messages = linter.verify(fixResult.output, config, filePath);
-        sourceCode = linter.getSourceCode();
-        if (messages.length === 0) break;
-      }
+      const fixedResult = verifyAndFix(linter, source, config, filePath, ruleIds, fixCreator);
 
       // Write the fixed source code to the file
-      if (fixResult && fixResult.fixed) {
+      if (fixedResult.fixed) {
         // eslint-disable-next-line no-await-in-loop, @typescript-eslint/no-non-null-assertion
-        await writeFile(filePath, fixResult.output);
+        await writeFile(filePath, fixedResult.output);
       }
     }
 
