617ae58831c9ef978176e9000f3a6eeea2f18b50
mizdra <pp.mizdra@gmail.com>
2024-01-03 02:55:25 +0900

    remove unused codes

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,63 +1,7 @@
 import { Linter, Rule, SourceCode } from 'eslint';
-import {
-  type FixableMaker,
-  type SuggestionFilter,
-  type FixToApplySuggestionsArgs,
-  type FixToDisablePerFileArgs,
-  type FixToDisablePerLineArgs,
-  type FixToMakeFixableAndFixArgs,
-  type FixToApplyAutoFixesArgs,
-  FixToConvertErrorToWarningPerFileArgs,
-} from './fix/index.js';
-import { fixRule, type FixRuleOption } from './fix-rule.js';
-import { preferAdditionShorthandRule } from './prefer-addition-shorthand-rule.js';
+import { type FixableMaker, type SuggestionFilter } from './fix/index.js';
 
-export { OVERLAPPED_PROBLEM_MESSAGE } from './fix-rule.js';
-
-export { FixRuleOption, type FixableMaker, type SuggestionFilter };
-
-export const eslintInteractivePlugin = {
-  rules: {
-    'fix': fixRule,
-    // for test
-    'prefer-addition-shorthand': preferAdditionShorthandRule,
-  },
-};
-
-/** For test */
-export type FixName =
-  | 'applyAutoFixes'
-  | 'disablePerLine'
-  | 'disablePerFile'
-  | 'convertErrorToWarningPerFile'
-  | 'applySuggestions'
-  | 'makeFixableAndFix';
-
-/** For test */
-export type FixArg<T extends FixName> = T extends 'applyAutoFixes'
-  ? FixToApplyAutoFixesArgs
-  : T extends 'disablePerLine'
-  ? FixToDisablePerLineArgs
-  : T extends 'disablePerFile'
-  ? FixToDisablePerFileArgs
-  : T extends 'convertErrorToWarningPerFile'
-  ? FixToConvertErrorToWarningPerFileArgs
-  : T extends 'applySuggestions'
-  ? FixToApplySuggestionsArgs
-  : T extends 'makeFixableAndFix'
-  ? FixToMakeFixableAndFixArgs
-  : never;
-
-/**
- * The type representing the fix to do.
- */
-export type Fix =
-  | { name: 'applyAutoFixes'; args: FixArg<'applyAutoFixes'> }
-  | { name: 'disablePerLine'; args: FixArg<'disablePerLine'> }
-  | { name: 'disablePerFile'; args: FixArg<'disablePerFile'> }
-  | { name: 'convertErrorToWarningPerFile'; args: FixArg<'convertErrorToWarningPerFile'> }
-  | { name: 'applySuggestions'; args: FixArg<'applySuggestions'> }
-  | { name: 'makeFixableAndFix'; args: FixArg<'makeFixableAndFix'> };
+export { type FixableMaker, type SuggestionFilter };
 
 /**
  * The type representing the additional information for the fix.
