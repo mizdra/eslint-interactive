0c9629543e1c8e309f05515b266e574ed8d7606b
mizdra <pp.mizdra@gmail.com>
2025-04-29 00:28:58 +0900

    refactor: print warning message without boxen

--- src/cli/log.ts
+++ src/cli/log.ts
@@ -1,11 +1,9 @@
-import boxen from 'boxen';
+import { styleText } from 'node:util';
 
 /**
  * Log a warning message to stderr
  * @param message The message to warn
  */
 export function warn(message: string) {
-  console.error(
-    boxen(message, { title: 'WARNING', borderColor: 'yellow', padding: { top: 0, left: 1, right: 1, bottom: 0 } }),
-  );
+  process.stderr.write(styleText('yellow', 'Warning') + ': ' + message + '\n');
 }
