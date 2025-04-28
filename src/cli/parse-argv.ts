350f3b478d369f3ac76c30912315d4843f72a1d9
mizdra <pp.mizdra@gmail.com>
2025-04-28 23:31:10 +0900

    fix: Fix an incorrect description of `--format`

--- src/cli/parse-argv.ts
+++ src/cli/parse-argv.ts
@@ -73,7 +73,7 @@ Options:
       --ext                          Specify JavaScript file extensions                                                                [array]
       --rulesdir                     Use additional rules from this directory                                                          [array]
       --ignore-path                  Specify path of ignore file                                                                      [string]
-      --format                       Specify the format to be used for the \`Display problem messages\` action [string] [default: "codeframe"]
+      --format                       Specify the format to be used for the \`Display problem messages\` action   [string] [default: "stylish"]
       --quiet                        Report errors only                                                             [boolean] [default: false]
       --cache                        Only check changed files                                                        [boolean] [default: true]
       --cache-location               Path to the cache file or directory                                                              [string]
