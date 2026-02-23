c9775f5c63f6bd93e96046f6a3afbd976a5dd84b
mizdra <pp.mizdra@gmail.com>
2026-02-23 16:12:42 +0900

    fix: fix an issue where the process did not terminate with code 1

--- bin/eslint-interactive.js
+++ bin/eslint-interactive.js
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
 // @ts-check
+import { run } from '../dist/index.js';
 
-import { spawnSync } from 'child_process';
-import { resolve } from 'path';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
-
-const dir = join(dirname(fileURLToPath(import.meta.url)));
-
-const scriptFile = resolve(dir, '_eslint-interactive.js');
-
-spawnSync('node', ['--unhandled-rejections=strict', scriptFile, ...process.argv.slice(2)], { stdio: 'inherit' });
+run({
+  argv: process.argv,
+}).catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
