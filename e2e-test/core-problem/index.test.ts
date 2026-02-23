f0d9af474f405f1c800ed576000c5db63a84df58
mizdra <pp.mizdra@gmail.com>
2026-02-23 16:31:56 +0900

    test: add E2E tests for ESLint core problem reporting

---
+++ e2e-test/core-problem/index.test.ts
@@ -0,0 +1,54 @@
+import { afterEach, test, expect } from 'vitest';
+import { createIFF } from '../../src/test-util/fixtures.ts';
+import dedent from 'dedent';
+import { type ChildProcessWithoutNullStreams, spawn } from 'child_process';
+import { createStreamWatcher } from '../../src/test-util/stream-watcher.ts';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const iff = await createIFF({
+  'src/invalid-syntax.js': dedent`
+    invalid syntax code;
+  `,
+  'eslint.config.js': dedent`
+    export default [
+      { rules: { 'semi': 'error' } },
+    ];
+  `,
+  'package.json': '{ "type": "module" }',
+});
+
+function waitForClose(child: ChildProcessWithoutNullStreams) {
+  return new Promise<number | null>((resolve) => {
+    if (child.exitCode !== null) resolve(child.exitCode);
+    child.on('close', (exitCode) => resolve(exitCode));
+  });
+}
+
+let child: ChildProcessWithoutNullStreams;
+afterEach(async () => {
+  child.kill();
+  await waitForClose(child);
+  await iff.reset();
+});
+
+test('report ESLint core problem details and exit with code 1', async () => {
+  child = spawn(
+    'node',
+    [
+      join(__dirname, '../../bin/eslint-interactive.js'),
+      'src',
+      // merge stderr to stdout
+      '2>&1',
+    ],
+    { shell: true, stdio: 'pipe', cwd: iff.rootDir },
+  );
+  const streamWatcher = createStreamWatcher(child.stdout, { debug: false });
+
+  const exitPromise = waitForClose(child);
+  await streamWatcher.match(/ESLint Core Problems are found/);
+  const exitCode = await exitPromise;
+  expect(exitCode).toBe(1);
+});
